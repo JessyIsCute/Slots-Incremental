7f07c63a932c7c1c11036a4263d231547d6ac084
JessyIsCute <lord.vraxxer@gmail.com>
2025-10-03 21:33:55 +0200

    fixed issues sure

--- Slots-Incremental/JS/symbols.js
+++ Slots-Incremental/JS/symbols.js
@@ -1,10 +1,3 @@
-const slotSymbols = [
-    new SlotSymbol(X, "X.png", 100, () => 0, true),
-    new SlotSymbol(Cherry, "Cherry.png", 50, () => 0, true),
-    new SlotSymbol(Diamond, "Diamond.png", 40, () => 0, true),
-    new SlotSymbol(Lemon, "Lemon.png", 30, () => 0, true),
-];
-
 class SlotSymbol {
     constructor(name,imgPath, weight, isUnlocked = false) {
         this.imgPath = imgPath;  // Image path or emoji
@@ -23,4 +16,11 @@ class SlotSymbol {
         return this.onWin(reels, index);
     }
 }
+
+const slotSymbols = [
+    new SlotSymbol("X", "X.png", 100, () => 0, true),
+    new SlotSymbol("Cherry", "Cherry.png", 50, () => 0, true),
+    new SlotSymbol("Diamond", "Diamond.png", 40, () => 0, true),
+    new SlotSymbol("Lemon", "Lemon.png", 30, () => 0, true),
+];
 
